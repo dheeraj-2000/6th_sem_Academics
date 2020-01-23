11c58754c36be45c831646bf51c604566c9506ed
dheeraj2000 <dheeraj12000@gmail.com>
2020-01-23 21:47:11 +0530

    added files

--- SocialNetworkAnalysis/HandsOn/Lecture5.R
+++ SocialNetworkAnalysis/HandsOn/Lecture5.R
@@ -1,12 +1,59 @@
 library(igraph)
 library(png)
-g = mkae_empty_graph(n = 50)
+m = make_empty_graph(n = 50)
 i =1
 while(i<=49){
-  g = g + edge(i, i+1)
+  m = m + edge(i, i+1)
   i = i+1
   fname = "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/SocialNetworkAnalysis/HandsOn/plot/plot"
   png(paste(fname, i, ".png", sep = ""),600, 600)
-  plot (g, vertex.color = "green", edge.color = "red", edge.arrow.size = 0.5, layout = layout_with_kk, rescale=FALSE, xlim=c(-5,5), ylim=c(-5,5), vertex.size=70, vertex.label.color = "black")
+  plot(m, layout= layout.grid)
   dev.off()
 }
+
+
+make_random <- function(num, prob){
+  g6 = make_empty_graph(n = num)
+  for(i in 1:(num-1)){
+    for(j in (i+1):num){
+      r = runif(1)
+      if(r<=prob){
+        g6 = g6+edge(i,j)
+        fname = "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/SocialNetworkAnalysis/HandsOn/plot2/plot"
+        png(paste(fname, i, ".png", sep = ""),600, 600)
+        plot(g6, layout= layout.grid)
+        dev.off()
+      }
+    }
+  }
+  return (g6)
+}
+g6 = make_random(45, 0.3)
+
+
+library(igraph)
+make_random <- function(num, prob){
+  g6 = make_empty_graph(n = num)
+  for(i in 1:(num-1)){
+    for(j in (i+1):num){
+      r = runif(1)
+      if(r<=prob){
+        g6 = g6+edge(i,j)
+        if(degree(g6)[i] >=1 && degree(g6)[i]<=2)
+          V(g6)$color[i] = "red"
+        if(degree(g6)[i] >=3 && degree(g6)[i]<=5)
+          V(g6)$color[i] = "blue"
+        else
+          V(g6)$color[i] = "yellow"
+        fname = "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/SocialNetworkAnalysis/HandsOn/plot2/plot"
+        png(paste(fname, i, ".png", sep = ""),600, 600)
+        plot(g6, layout= layout.grid, vertex.size = igraph::degree(g6)*3)
+        dev.off()
+      }
+    }
+  }
+  return (g6)
+}
+g6 = make_random(50, 0.1)
+
+
