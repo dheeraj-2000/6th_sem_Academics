ee92578e5ada3af7be2adcd9161bf3865ae89c57
dheeraj2000 <dheeraj12000@gmail.com>
2020-02-08 14:43:07 +0530

    updated sociopattern

--- SocialNetworkAnalysis/HandsOn/ALLINONE.R
+++ SocialNetworkAnalysis/HandsOn/ALLINONE.R
@@ -1,36 +1,43 @@
 library(igraph)
 #####################READING THE FRIENDSHIP GRAPH############################33
 g = read_graph("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/SocialNetworkAnalysis/HandsOn/Friendship-network_data_2013.csv", format = "edgelist")
-V(g)
-E(g)
+V(g)                      # GET THE NUMBER OF VERTICES
+E(g)                      # GET THE NUMBER OF EDGES
 tkplot (g, vertex.label = NA, vertex.color = "green", reciprocity= TRUE, edge.color = "red", edge.arrow.size = 0.5, layout = layout_with_kk, rescale=FALSE, xlim=c(-5,5), ylim=c(-5,5), vertex.size=2, vertex.label.color = "black")
+###############  PROPERTY ####
 reciprocity(g) 
 
+
+################ MAKING THE GRAPH AS A UNDIRECTED ################33
 g1 = as.undirected(g, mode = "mutual")
 V(g1)   #vertex for undirected
-E(g1)
+E(g1)   # GET THE NUMBER OF EDGES FOR UNDIRECTED
 tkplot (g1, vertex.size = 2, vertex.label = NA, edge.color = "red", vertex.color = "green", edge.width = 0.5, layout = layout_with_kk)
+
+################ COMPONENTS IN THE UNDIRECTED GRAPH ####################3
 components(g1)
 comps = decompose(g1)
 comps
 tesla = comps[[2]]
 tkplot(tesla)
 
-g3 = delete.vertices (g1, V(g1) [V(g1)$degree == 0])
-tkplot (g1, vertex.size = 2, vertex.label = NA, edge.color = "red", vertex.color = "green", edge.width = 0.5, layout = layout_with_kk)
 
-# Plot degree distribution
-t = table(degree(g3))
+# Plot degree distribution FOR UNDIRECTED GRAPH G1
+t = table(degree(g1))
 plot(t / sum(t),xlab="Degree", ylab="Frequency")
 
+
+############################ READING METADATA GRAPH ################################
 g2 = read.csv("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/SocialNetworkAnalysis/HandsOn/metadata_2013.txt", header = F, sep = "\t")
-V(g)
-E(g)
+V(g)                 #VERTICES IN THE METADATA
+E(g)                  #EDGES IN THE METADATA
+
 
 V(g1)$degree = igraph::degree (g1)
 V(g1)$degree
 
-#getting betweeness
+#################### FOR UNDIRECTED GRAPH G1
+#getting betweeness OF UNDIRECTED GRAPH G1
 V(g1)$betweeness = igraph::betweenness(g1)
 V(g1)$betweeness
 
@@ -38,6 +45,7 @@ V(g1)$betweeness
 V(g1)$closeness = igraph::closeness(g1)
 V(g1)$closeness 
 
+###################### GETTING TOP NODES FOR BETWEEN,CLOSENESS AND DEGREE ###################
 Topdeg = order(V(g1)$degree,decreasing=T)[1:5]
 Topclose = order(V(g1)$closeness,decreasing=T)[1:5]
 Topbw = order(V(g1)$betweenness,decreasing=T)[1:5]
@@ -45,6 +53,7 @@ Topbw = order(V(g1)$betweenness,decreasing=T)[1:5]
 Top = intersect(Topdeg,Topclose)
 Top = intersect(Top,Topbw)
 
+################### PLOT WHILE CONISDERING ALL ABOVE 3 CASES
 plot (g1, mark.groups=Top, mark.col="yellow", vertex.size = 2, vertex.label = NA, edge.color = "red", vertex.color = "black", edge.width = 0.5, layout = layout_with_kk)
 
 V(g1)[g1 [,1]]$gender = g1 [,3]
@@ -59,7 +68,7 @@ tkplot (g4, vertex.size = 5, vertex.label = NA, edge.color = "black", vertex.col
 
   
 
-
+################# GET THE CLOSENESS, BETWEENESS TOPNODES OF COMPONENT (GRAPHNEMAE TESLA) ##########
 V(tesla)$degree = igraph::degree (tesla)
 V(tesla)$degree
 
@@ -78,8 +87,8 @@ plot (tesla_dist, xlab = "degree", ylab = "frequency")
 Topdeg = order(V(tesla)$degree,decreasing=T)[1:5]
 Topclose = order(V(tesla)$closeness,decreasing=T)[1:5]
 Topbw = order(V(tesla)$betweenness,decreasing=T)[1:5]
-
 Top = intersect(Topdeg,Topclose)
 Top = intersect(Top,Topbw)
+
 plot (g, mark.groups=Top, mark.col="yellow", vertex.size = 2, vertex.label = NA, edge.color = "red", vertex.color = "green", edge.width = 0.5, layout = layout_with_kk)
 
