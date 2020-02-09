bd9ae156f7730573a2291b62267f5aea0bdd214d
dheeraj2000 <dheeraj12000@gmail.com>
2020-02-09 23:14:48 +0530

    updated lab1

--- DataScience/Analysis.R
+++ DataScience/Analysis.R
@@ -1,4 +1,5 @@
 library(tabulizer)
+library(dplyr)
 original_data <- "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/CRS_2016.pdf"
 read_pdf <- extract_areas(data.file,pages = 11,output = "data.frame",header = F)
 read_pdf
@@ -7,3 +8,10 @@ df
 colnames(read_pdf) <- c("year","NVER_Live_Births","NVER_Still_Births","NVER_Deaths","CRS_Births","CRS_deaths","% of CRS vs SRS _Births",
                    "% of CRS vs SRS_deaths")
 read_pdf
+
+read_pdf %>% summarise(meanliveBirths = mean(NVER_Live_Births))
+read_pdf %>% summarise(medianliveBirths = median(NVER_Live_Births))
+read_pdf %>% summarise(maxliveBirths = max(NVER_Live_Births))
+read_pdf %>% summarise(minliveBirths = min(NVER_Live_Births))
+read_pdf %>% summarise(stdliveBirths = sd(NVER_Live_Births)) 
+read_pdf %>% summarise(IQRliveBirths = IQR(NVER_Live_Births))
