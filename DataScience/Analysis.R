c9a26bf58c3f4aa28de5f7e5a881a101f3f05cbd
dheeraj2000 <dheeraj12000@gmail.com>
2020-02-09 23:08:56 +0530

    updated lab1

--- DataScience/Analysis.R
+++ DataScience/Analysis.R
@@ -1,5 +1,9 @@
 library(tabulizer)
-read_pdf <- extract_areas("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/CRS_2016.pdf", pages = 11, header = F)
-req_table <- my_table[[1]]  
-df <- as.data.frame(req_table, header = T)
+original_data <- "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/CRS_2016.pdf"
+read_pdf <- extract_areas(data.file,pages = 11,output = "data.frame",header = F)
+read_pdf
+read_pdf <- as.data.frame(read_pdf)
 df
+colnames(read_pdf) <- c("year","NVER_Live_Births","NVER_Still_Births","NVER_Deaths","CRS_Births","CRS_deaths","% of CRS vs SRS _Births",
+                   "% of CRS vs SRS_deaths")
+read_pdf
