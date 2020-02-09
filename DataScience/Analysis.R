818afaed2a304df053e59f9ab30895ad2dbc7466
dheeraj2000 <dheeraj12000@gmail.com>
2020-02-10 00:01:53 +0530

    updated lab1

--- DataScience/Analysis.R
+++ DataScience/Analysis.R
@@ -5,13 +5,23 @@ read_pdf <- extract_areas(data.file,pages = 11,output = "data.frame",header = F)
 read_pdf
 read_pdf <- as.data.frame(read_pdf)
 df
-colnames(read_pdf) <- c("year","NVER_Live_Births","NVER_Still_Births","NVER_Deaths","CRS_Births","CRS_deaths","% of CRS vs SRS _Births",
-                   "% of CRS vs SRS_deaths")
+colnames(read_pdf) <- c("year","event_Reg_LBirth","event_Reg_SBirth","event_Reg_Deaths","CRS_Births","CRS_deaths","%ofCRS_SRS_Births",
+                   "%ofCRS_SRS_Deaths")
 read_pdf
 
-read_pdf %>% summarise(meanliveBirths = mean(NVER_Live_Births))
-read_pdf %>% summarise(medianliveBirths = median(NVER_Live_Births))
-read_pdf %>% summarise(maxliveBirths = max(NVER_Live_Births))
-read_pdf %>% summarise(minliveBirths = min(NVER_Live_Births))
-read_pdf %>% summarise(stdliveBirths = sd(NVER_Live_Births)) 
-read_pdf %>% summarise(IQRliveBirths = IQR(NVER_Live_Births))
+write.csv(read_pdf, "/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/new_data.csv")
+
+nw_read <- read.csv("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/new_data.csv")
+nw_read
+nw_read$X <- NULL
+nw_read
+tail(nw_read, -11)
+
+min(nw_read[["NVER_Live_Births"]])
+max(nw_read[["NVER_Live_Births"]])
+mean(nw_read[["NVER_Live_Births"]])
+median(nw_read[["NVER_Live_Births"]])
+var(nw_read[["NVER_Live_Births"]])
+sd(nw_read[["NVER_Live_Births"]])
+IQR(nw_read[["NVER_Live_Births"]])
+
