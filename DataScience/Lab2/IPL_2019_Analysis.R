35a2faf6de78b1eeb5570e3ad1a2941a56dd0e6a
dheeraj2000 <dheeraj12000@gmail.com>
2020-02-16 15:22:35 +0530

    added lab2 folder and systemized

---
+++ DataScience/Lab2/IPL_2019_Analysis.R
@@ -0,0 +1,61 @@
+library(tabulizer)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(magrittr)
+library(tidyr)
+
+matches <- read.csv("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/Lab2/matches.csv", stringsAsFactors = FALSE)
+
+data <- read.csv("/home/dheeraj/Desktop/Lecture/6th_sem_Academics/DataScience/Lab2/deliveries.csv", stringsAsFactors = FALSE)
+
+matches <- matches[,-18]
+data$wickets <- as.numeric(ifelse(data$player_dismissed =="" ,"",1))
+
+#number of matches in the dataset
+summarize(matches,no_of_matches = n())
+# Output = 60
+
+#which Team won by maximum runs
+max_run <- matches[which.max(matches$win_by_runs),]
+select(max_run, winner, win_by_runs)
+# Sunrisers Hyderabad by 118 runs
+
+#which Team won by maximum runs
+max_run <- matches[which.max(matches$win_by_wickets),]
+select(max_run, winner, win_by_wickets)
+# Sunrisers Hyderabad by 9 wicket
+
+
+teams <- data %>% select(batting_team)%>%
+  distinct()
+teams <- rename(teams, team = batting_team) 
+teams
+s_team <- c("RCB","CSK","SRH","KKR","DC","MI","KXIP","RR")
+teams <- cbind(teams, s_team)
+player_of_match <- matches%>% select(id,player_of_match,season) %>%
+  distinct()
+player_of_match <- rename(player_of_match, player=player_of_match)
+
+matches$city <- as.character(matches$city)
+matches$city[matches$city==""] <- "Dubai"
+venue_city <- matches %>%
+  select(city)%>%
+  distinct()
+
+
+########################## Dissmissal type and number of dismissal ###################################
+dismissal <- data%>%
+  left_join(matches, by=c("match_id"="id"))%>%
+  left_join(teams,by=c("batting_team"="team"))%>%
+  filter(dismissal_kind!="")%>%
+  group_by(season,dismissal_kind,s_team)%>%
+  summarize(wickets =n())
+ggplot(dismissal,aes(x=dismissal_kind,y=wickets,colour=as.factor(season),fill=as.factor(season)))+
+  geom_bar(position = "stack", show.legend = TRUE, width =.6,stat="identity")+
+  theme(legend.position="bottom")+
+  coord_flip()+
+  theme(legend.direction = "horizontal") +
+  scale_y_continuous(name="wickets")+
+  scale_x_discrete(name="dismissal kind")+
+  ggtitle("Breakdown of dismissal type")
