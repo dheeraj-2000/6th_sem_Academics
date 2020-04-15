eea22f32721630191e6c89d80c26d2947e33f94f
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-15 20:42:01 +0530

    updated DA lab

--- DataScience/Lab3/time_series.R
+++ DataScience/Lab3/time_series.R
@@ -3,3 +3,11 @@ data("sunspot.month")
 sunspot.month
 View(sunspot.month)
 plot(sunspot.month)
+
+
+####time series object
+
+time_series <- ts(sunspot.month, start=1749 ,frequency = 12)
+time_series
+is.ts(time_series)
+ts.plot(time_series)
