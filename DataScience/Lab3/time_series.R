52430654ff14d88b1dca87cd6810f8cab54b0ee1
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-16 17:43:45 +0530

    added dataset and initpre declarations

--- DataScience/Lab3/time_series.R
+++ DataScience/Lab3/time_series.R
@@ -4,11 +4,21 @@ sunspot.month
 View(sunspot.month)
 plot(sunspot.month)
 
+library(ggplot2)
+library(Metrics)
+library(forecast)
+library(reshape)
+data("sunspot.month")
+sunspot.month
 
-####time series object
 
-time_series <- ts(sunspot.month, start=1749 ,frequency = 12)
-time_series
-is.ts(time_series)
-ts.plot(time_series)
+#start end time frequency cycle - The deltat() function returns the fixed time interval between observations and the frequency() function returns the number of observations per unit time. Finally, the cycle() function returns the position in the cycle of each observation
+start(sunspot.month)
+end(sunspot.month)
+time(sunspot.month)
+cycle(sunspot.month)
+deltat(sunspot.month)
+
+
+
 
