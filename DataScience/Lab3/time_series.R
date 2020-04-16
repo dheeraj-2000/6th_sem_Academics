03b0d1a775d60e807f62702355ebcb3b895b22df
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-16 19:04:07 +0530

    added seasonality

--- DataScience/Lab3/time_series.R
+++ DataScience/Lab3/time_series.R
@@ -22,31 +22,39 @@ deltat(sunspot.month)
 
 ####time series object
 
-time_series <- ts(mdeaths, start=1974 ,frequency = 12)
-time_series
+time_series1 <- ts(mdeaths, start=1974 ,frequency = 12)
+time_series1
 #check whether it is a ts object
-is.ts(time_series)
-ts.plot(time_series, main="Sunspot",ylab ="Monthly Total mean Sunspot number")
-abline(reg = lm(time_series~time(time_series)),col="green")
+is.ts(time_series1)
+ts.plot(time_series1, main="Sunspot",ylab ="Monthly Total mean Sunspot number")
+abline(reg = lm(time_series1~time(time_series)1),col="green")
 
 #This property can stabilize variability when a series exhibits increasing variability over time. It may also be used to linearize a rapid growth pattern over time
 
 #Q3  -  Monthly mean values
 
-plot(aggregate(time_series,FUN = mean))
+plot(aggregate(time_series1,FUN = mean))
 
 #Q4 -  Boxplot Monthly
 
-boxplot(time_series~cycle(time_series),xlab="Month",ylab = "Sunspot Number",main = "Sunspot")
+boxplot(time_series1~cycle(time_series1),xlab="Month",ylab = "Sunspot Number",main = "Sunspot")
 
 #Q5 - decompose using stl and find TREND
 
 
-stlts <- stl(time_series, s.window = "periodic")
+stlts <- stl(time_series1, s.window = "periodic")
 plot(stlts)
 
 
 stlts$trend <- stlts$time.series[,2]
 plot(stlts$trend)
+
+
+# Trend is downward
+
+# Q6  - Seasonality
+
+stlts$seasonal <- stlts$time.series[,1]
+plot(stlts$seasonal)
 
 
