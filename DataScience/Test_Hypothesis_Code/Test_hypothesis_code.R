4c0559cf6b354f3941f90ee08a70dc744bbaced9
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-25 11:08:49 +0530

    added Test of Hypothesis code

--- DataScience/Test_Hypothesis_Code/Test_hypothesis_code.R
+++ DataScience/Test_Hypothesis_Code/Test_hypothesis_code.R
@@ -2,3 +2,15 @@
 #### Here Given values are MEAN, SSAMPLE SIZE and STANDARD DEVIATION of Both Samples #############
 
 ##### Considering the Examples given in DR. ATHE's TESTING OF HYPOTHESIS Pg.No. 35#####
+
+# Given DATA for Field No 1
+
+field_1_Sample_Size <- 60
+StandDeviation_1 <- 1.15
+field_1_Sample_MEAN <- 18.5
+
+# Given DATA for Field No 2
+
+field_2_Sample_Size <- 60
+StandDeviation_2 <- 1.15
+field_2_Sample_MEAN <- 20.3
