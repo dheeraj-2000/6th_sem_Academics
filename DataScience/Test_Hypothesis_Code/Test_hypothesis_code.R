30665d6e29ea947056f5ea61909ca412f7932e39
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-25 11:02:17 +0530

    added Test of Hypothesis code

--- DataScience/Test_Hypothesis_Code/Test_hypothesis_code.R
+++ DataScience/Test_Hypothesis_Code/Test_hypothesis_code.R
@@ -1,2 +1,4 @@
 ################# Two sample Z-Test or Test of significant for difference of means ###############
 #### Here Given values are MEAN, SSAMPLE SIZE and STANDARD DEVIATION of Both Samples #############
+
+##### Considering the Examples given in DR. ATHE's TESTING OF HYPOTHESIS Pg.No. 35#####
