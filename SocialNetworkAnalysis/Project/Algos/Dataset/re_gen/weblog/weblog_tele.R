7e7a2a0ad9ce5837d33d1c46265d669f69b529ea
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-12 04:00:37 +0530

    UPDATED plots

--- SocialNetworkAnalysis/Project/Algos/Dataset/re_gen/weblog/weblog_tele.R
+++ SocialNetworkAnalysis/Project/Algos/Dataset/re_gen/weblog/weblog_tele.R
@@ -1,10 +1,8 @@
 library(igraph)
 library(ggplot2)
 
-degree_ <- c(4)
-y1 <- c(
-        1
-)
+degree_ <- c(4,11)
+y1 <- c(1,1)
 
 shel <- c(1,
           2,
@@ -16,7 +14,7 @@ shel <- c(1,
           8,
           9,
           10,
-          11,
+          11
           
 )
 y2 <- c(0.07471264368,
@@ -31,7 +29,7 @@ y2 <- c(0.07471264368,
         0.9942528736,
         1
 )
-plot(degree_,y1,type="l",col="red", xlim=c(.9, 9), ylim = c(0.5, 1.01),  xlab="Number of steps taken",ylab="Cumulative fraction of instances", main = "Comparison of algorithm for weblog using teleportation") + 
+plot(degree_,y1,type="l",col="red", xlim=c(0, 13), ylim = c(0.5, 1.01),  xlab="Number of steps taken",ylab="Cumulative fraction of instances", main = "Comparison of algorithm for weblog using teleportation") + 
   lines(shel,y2,col="blue")
 legend("bottomright", legend=c("Shell Based", "Degree based"),lty=1:1, cex=0.8 ,
        col=c("blue", "red"))
