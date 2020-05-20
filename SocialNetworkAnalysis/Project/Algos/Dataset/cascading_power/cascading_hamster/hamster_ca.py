eef5657637654d546d42aabeaebbfa7b8f0e27e6
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-20 11:39:19 +0530

    updated code for hamster cascade

--- SocialNetworkAnalysis/Project/Algos/Dataset/cascading_power/cascading_hamster/hamster_ca.py
+++ SocialNetworkAnalysis/Project/Algos/Dataset/cascading_power/cascading_hamster/hamster_ca.py
@@ -1,3 +1,30 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+df=pd.read_csv("/home/dheeraj/my_projects/my_project_env/practice/6th_sem_Academics/SocialNetworkAnalysis/Project/Algos/Dataset/cascading_power/cascading_hamster/hamster_cascade.csv",encoding = "ISO-8859-1")
+
+
+
+df["meanplussd"] = df["powmean"] + df["powsd"]
+df["meanminusssd"] = df["powmean"] - df["powsd"]
+
+x = df['s']
+y1 = df['meanplussd']
+y2 = df['meanminusssd']
+z = df['powmean']
+
+
+# fig = plt.figure(figsize=(10, 8))
+
+plt.plot(x,z, color = "black", label = "mean line")
+plt.plot(x,y1, color = "red", label = "for standard deviation")
+plt.plot(x,y2, color = "red")
+plt.fill_between(x, y1, y2, where=None,color = "yellow", interpolate=False, step=None, data=None, alpha='0.3')
+plt.xlabel('Shell Number')
+plt.ylabel('Cascading Power')
+plt.title('Cascading power in Hamster Network')
+plt.legend()
+plt.show()
+# plt.savefig('cascading_haster.png', dpi=800)
